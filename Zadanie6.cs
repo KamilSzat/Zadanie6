107500bdc00deba6ec7577b4fd09ca0e9c4666d7
KamilSzat <uT_3776@zsp3zamosc.pl>
2025-05-14 08:50:00 +0200

    Zadanie 6

---
+++ Zadanie6.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie51
+{
+    public class Zadanie6
+    {
+        public int CountOccurrences(int[] array, int element)
+        {
+            return array.Count(e=> e==element);
+        }
+    }
+}
